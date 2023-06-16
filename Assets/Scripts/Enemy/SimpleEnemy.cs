02d519259171ebe9e108ef72761a18d4b36dde38
Konstantin-Mur <murashko.konst.andr@gmail.com>
2023-06-16 20:55:51 +0300

    Add scene, add scriptes

---
+++ Assets/Scripts/Enemy/SimpleEnemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : Enemy, IMove, IShot, IDead
+{
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float moveSpeed;
+
+    public void Dead()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Shot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        SetInterfaces(this, this, this);
+    }
+
+}
